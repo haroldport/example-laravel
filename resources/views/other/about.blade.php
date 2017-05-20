e5497feaa2faa82ac2106549afd3700b8eb1b341
Harold Portocarrero <harold.port@gmail.com>
2017-05-19 22:04:26 -0500

    aplicacion basica de laravel sin conexion a base de datos

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">About me</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies quis nunc et tempus. Phasellus ultricies vehicula vulputate. Duis pharetra non purus in feugiat. Praesent lectus leo, pellentesque eu ante vitae, fringilla malesuada lacus. Quisque porttitor ex et nisl scelerisque blandit in vitae mi. Ut lobortis nunc a arcu condimentum, eget imperdiet augue rutrum. Morbi neque dolor, ultricies quis odio non, pretium pretium lectus. Ut tristique ligula vitae sagittis aliquet. Nulla aliquam malesuada sagittis. Etiam malesuada non lectus eu efficitur. Etiam eu arcu nisi. Integer convallis vel ipsum ac suscipit. In accumsan vitae turpis sed tristique.</p>
+        </div>
+    </div>
+@endsection
